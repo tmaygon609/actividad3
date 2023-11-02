26ea6f96bf2fc292e17151590d756e6c483de411
tmaygon609 <tmaygon609@iesalmudeyne.es>
2023-11-02 14:11:52 +0100

    Modificación hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.prinln("Hola, ¿como están los alumnos de 2º DAW?")
+        System.out.prinln("Hola, ¿como están los alumnos de 2º DAW? o no estais?")
     }
 }
